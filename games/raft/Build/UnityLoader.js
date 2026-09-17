c21a4fc696b6f549d2783ecefd31071e8c67154b
idkmod12 <idezmonsp@gmail.com>
2026-09-17 18:07:01 -0400

    Allow Raft Unity build on touch browsers

--- games/raft/Build/UnityLoader.js
+++ games/raft/Build/UnityLoader.js
@@ -1,12 +1,8 @@
 var UnityLoader = UnityLoader || {
     compatibilityCheck: function (e, t, r) {
-        UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.mobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : ["Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : t() : e.popup("Your browser does not support WebGL", [{
+        // Unity's mobile warning blocks touch devices behind a modal. WebGL works
+        // on iPadOS, so continue directly whenever the browser exposes WebGL.
+        UnityLoader.SystemInfo.hasWebGL ? t() : e.popup("Your browser does not support WebGL", [{
             text: "OK",
             callback: r
         }])
